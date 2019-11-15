8f0ad517817e34cd6dc7910555b907c1f7372108
Killawad21 <02logan.pennock@gulllakecs.org>
2019-11-15 09:43:00 -0500

    Add countBlocks() function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,22 @@ package com.company;
 
 public class Main {
 
+
+        public static void countBlocks(int levels) {
+            int total = 0;
+            for (int i = 1; i <= levels; i++) {
+                total = total + (i * i);
+            }
+            System.out.println(total);
+        }
     public static void main(String[] args) {
-        for(int i=0; i<3; i++){
+        for (int i = 0; i < 3; i++) {
             System.out.println("Outer Loop: " + i);
-            for(int j=0; j<5; j++){
+            for (int j = 0; j < 5; j++) {
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!");
             }
         }
+        countBlocks(10);
     }
+
 }

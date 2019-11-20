be2136dcbb3e29a058875cca4135909983436865
Killawad21 <48028251+Killawad21@users.noreply.github.com>
2019-11-20 10:20:43 -0500

    Started rollDice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
 public class Main {
-
+    public static void rollASix(){
+        int rollDice = 0;
+        
+        while (rollDice != 6){
+            rollDice = (int) (Math.ran
+        
     public static void main(String[] args) {
         for(int i=0; i<3; i++){
             System.out.println("Outer Loop: " + i);
